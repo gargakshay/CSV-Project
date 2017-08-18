bd2b24fb793f84a44b41dbb4e8677af77ded607d
akshay garg <gargakshay2010@gmail.com>
2017-08-18 14:25:48 +0530

    Added port

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var index = require('./routes/index');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, 'angular/dist/')));
 app.use('/', index);
 
-app.listen(5000, function(){
-    console.log("Server Started");
+app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'));
 })
