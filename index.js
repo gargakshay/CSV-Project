66ca7b2202b576f1477119c099c08ae7437b9df7
akshay garg <gargakshay2010@gmail.com>
2017-08-18 19:49:01 +0530

    Testing

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'angular1/dist/')));
 console.log("path******************88", path.join(__dirname, 'angular1/dist/'));
 // app.use('/', index);
 
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
 
 app.get('/', function(req, res, next){
     res.send('index.html11111111');
