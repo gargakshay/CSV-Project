eadd9dda6115d1d5fa222bb71d384d1479fee267
akshay garg <gargakshay2010@gmail.com>
2017-08-18 19:36:09 +0530

    Testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var multer  = require('multer');
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  }
-});
+// var storage =   multer.diskStorage({
+//   destination: function (req, file, callback) {
+//     callback(null, './uploads');
+//   }
+// });
 
-var upload = multer({ storage : storage }).any();
+// var upload = multer({ storage : storage }).any();
 
 var csv = require('csvtojson');
 var path = require('path');
@@ -26,34 +26,34 @@ app.get('/', function(req, res, next){
     res.send('index.html11111111');
 });
 router.post('/upload', function(req, res, next){
+    res.send('index.html11111111');
+    // upload(req, res, function(err) {
 
-    upload(req, res, function(err) {
-
-        if(err){
-            res.sendStatus(400);
-            return;
-        }
+    //     if(err){
+    //         res.sendStatus(400);
+    //         return;
+    //     }
         
-        var arrFiles = req.files;
-        var arrData = [];
+    //     var arrFiles = req.files;
+    //     var arrData = [];
 
-        for(let i = 0; i < arrFiles.length; i++){
-            //Converting CSV to JSON
-            csv()
-            .fromFile(arrFiles[i].path)
-            .on("end_parsed", function(jsonObj){
-                arrData.push({data: jsonObj, filename: arrFiles[i].originalname});
+    //     for(let i = 0; i < arrFiles.length; i++){
+    //         //Converting CSV to JSON
+    //         csv()
+    //         .fromFile(arrFiles[i].path)
+    //         .on("end_parsed", function(jsonObj){
+    //             arrData.push({data: jsonObj, filename: arrFiles[i].originalname});
                 
-                // When file is last then send Response
-                if(i == arrFiles.length - 1){
-                    res.send(arrData);
-                }
-            })
-            .on('error',(err)=>{
-                res.sendStatus(400);
-            });        
-        }
-    });
+    //             // When file is last then send Response
+    //             if(i == arrFiles.length - 1){
+    //                 res.send(arrData);
+    //             }
+    //         })
+    //         .on('error',(err)=>{
+    //             res.sendStatus(400);
+    //         });        
+    //     }
+    // });
 });
 
 
